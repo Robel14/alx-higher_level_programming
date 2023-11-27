0ef80ee1aa0d433bb67ab1b12a936ff2b2f14db8
Robel14 <robelaylew2021@gmail.com>
2023-11-27 07:55:36 -0800

    102-magic_calculation.py

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    return(98 + (a ** b))
+    3           0 LOAD_CONST               1 (98)
+              3 LOAD_FAST                0 (a)
+              6 LOAD_FAST                1 (b)
+              9 BINARY_POWER
+             10 BINARY_ADD
+             11 RETURN_VALUE
+             return(98 + (a ** b))
